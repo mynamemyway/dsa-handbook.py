93217b458c3362adc24ca32d56555257a9982fc9
Aleksandr Samokhvalov <samokhvaloff.on@gmail.com>
2025-10-07 12:41:09 +0300

    up

--- less1.py
+++ less1.py
@@ -1,2 +1,8 @@
 n = int(input())
-# Расписываем все диапазоны, заканчиваем else: «ошибка ввода»
+
+l = [list(range(1, i + 1)) for i in range(1, n + 1)]
+
+# for i in range(1, n + 1):
+#     l.append(list(range(1, i + 1)))
+    
+print(*l, sep='\n')
