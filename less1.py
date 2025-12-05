2a2d8df74d3581240b61790e3fe64c87b3b7ef79
Aleksandr Samokhvalov <samokhvaloff.on@gmail.com>
2025-12-06 01:44:30 +0300

    up

--- less1.py
+++ less1.py
@@ -6,3 +6,16 @@ l = [list(range(1, i + 1)) for i in range(1, n + 1)]
 #     l.append(list(range(1, i + 1)))
     
 print(*l, sep='\n')
+
+
+n = int(input())
+
+matrix = [[0] * n for n in range(n)]
+
+for row in matrix:
+    print(*row)
+    
+# 0
+# 0 0
+# 0 0 0
+# 0 0 0 0
