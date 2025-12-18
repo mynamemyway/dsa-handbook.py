9f87780eff4b7946643315f6e17e39ca681d629f
Aleksandr Samokhvalov <samokhvaloff.on@gmail.com>
2025-12-18 13:40:56 +0300

    up

--- lessons/less_0.py
+++
@@ -1,7 +0,0 @@
-words_l = [word.strip('.,!?:;-') for word in input().lower().split()]
-
-words_d = {}
-for word in words_l:
-    words_d.setdefault(words_l.count(word), set()).add(word)
-
-print(*sorted(words_d[min(words_d)]))
